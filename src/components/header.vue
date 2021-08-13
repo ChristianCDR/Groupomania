7d3a5919860c1aeee7761d624e49f68a94885e74
Christian CLAUDE DIT REGENT <christian.cdr@outlook.fr>
2021-08-13 21:19:48 +0200

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'header',
+  components: {
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')

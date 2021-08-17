c6ecbd1818865cee16eea172219a0deb6c240fb8
Christian CLAUDE DIT REGENT <christian.cdr@outlook.fr>
2021-08-17 23:59:20 +0200

    hash password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,14 +1,23 @@
 const models= require('../models');
 const usersModel=models.user;
+const bcrypt= require('bcrypt');
 
-exports.saveInDB=(req,res, next)=>{
-  const userInfos={
-    ...req.body 
-  }
-  usersModel.create(userInfos)
-  .then(result=>{res.status(201).json({
-    message: 'User enregistré!',
-    user: result
-  })})
-  .catch(error=>{res.status(500).json({message: error})});
+exports.signup=(req,res, next)=>{
+  bcrypt.hash(req.body.motDePasse, 10)
+  .then(hash=>{
+    const userInfos={
+      nom: req.body.nom,
+      prenom: req.body.prenom,
+      email: req.body.email,
+      motDePasse: hash
+    }
+    usersModel.create(userInfos)
+    .then(result=>{res.status(201).json({
+      message: 'User enregistré!',
+      user: result
+    })})
+    .catch(error=>{res.status(500).json({message: error})});
+  })
+  .catch(error=>{res.status(500).json({messagefromHashCatch: error})})
+  
 }
